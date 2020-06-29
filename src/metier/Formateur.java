05c73be4233db500ce7a43d55fadd285e7afc911
Hugo <hgconrol7@gmail.com>
2020-06-29 09:59:45 +0200

    modif Formateur

--- src/metier/Formateur.java
+++ src/metier/Formateur.java
@@ -2,6 +2,6 @@ package metier;
 
 public class Formateur {
 	public void formateur() {
-		System.out.println("rien à dire");
+		System.out.println("rien à dire. Mais il faut faire une modif");
 	}
 }
